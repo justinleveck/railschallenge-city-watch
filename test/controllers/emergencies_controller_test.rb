d4a107057120d5795e4f1a721f8e5afe9d3ff4cd
Justin Leveck <jleveck@gmail.com>
2015-09-23 14:56:41 -0700

    Remove unused tests

--- test/controllers/emergencies_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EmergenciesControllerTest < ActionController::TestCase
-end
--- test/helpers/emergencies_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EmergenciesHelperTest < ActionView::TestCase
-end
